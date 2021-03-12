a496220f0c036f5b52b85faf198ca5b9315c1717
Gilang Hardadi <hardadi@ad.vti.bund.de>
2021-03-12 14:54:47 +0100

    Push 12.03.2021 - ghardadi
    Add Update Function in Sample_bio Code

--- commercial/03_input_program/frm_length_code.py
+++ commercial/03_input_program/frm_length_code.py
@@ -12,7 +12,6 @@ from PyQt5.QtWidgets import *
 import frm_length
 import csv
 
-#import account
 from reference import Reference as ref
 
 
@@ -22,6 +21,8 @@ class WeightTableModel(QAbstractTableModel):
         super(WeightTableModel, self).__init__()
         
         self.haul_uid = ref.haul_uid
+        self.weight_uid = None
+        
         cur = ref.connection.cursor()
         
         sql_statement = """SELECT * FROM com_new_final.sample_weight WHERE
@@ -52,6 +53,9 @@ class WeightTableModel(QAbstractTableModel):
         if orientation == Qt.Horizontal:
             return self.header[section]
         return int(section+1)
+    
+    def flags(self, index):
+        return Qt.ItemFlags(QAbstractTableModel.flags(self, index)|Qt.ItemIsEditable)
 
     def data(self, index, role = Qt.DisplayRole):
         if not index.isValid() or not (0 <= index.row() < len(self.__data)) or index.column() >= len(self.header):
@@ -64,6 +68,27 @@ class WeightTableModel(QAbstractTableModel):
             return data_row[column]
 
         return None
+    
+    def setData(self, index, value, role=Qt.EditRole):
+        if not index.isValid():
+            return False
+        row = index.row()
+        if index.column() > 0:
+            self.__data[row][index.column()] = value
+            self.__data[row][-1] = True
+            self.dataChanged.emit(index, index)
+            return True
+        return False
+    
+    def insertRows(self, row, count, parent=QModelIndex(), data=[]):
+        self.beginInsertRows(parent, row, row+len(data))
+        for d in data:
+            d.extend([None, False])
+            print('insertRows : {0}'.format(d))
+            self.__data.insert(row, d)
+        self.endInsertRows()
+        print('insertRows : {0}'.format(self.__data))
+        return True
 
 class LengthTableModel(QAbstractTableModel):
 
@@ -71,6 +96,8 @@ class LengthTableModel(QAbstractTableModel):
         super(LengthTableModel, self).__init__()
         
         self.weight_uid = ref.weight_uid
+        self.length_uid = None
+        
         cur = ref.connection.cursor()
         
         sql_statement = """SELECT * FROM com_new_final.sample_length WHERE
@@ -111,7 +138,41 @@ class LengthTableModel(QAbstractTableModel):
         if role == Qt.DisplayRole or role == Qt.EditRole:
             return data_row[column]
 
-        return None
+        return None    
+    
+    def flags(self, index):
+        return Qt.ItemFlags(QAbstractTableModel.flags(self, index)|Qt.ItemIsEditable)
+
+    def setData(self, index, value, role=Qt.EditRole):
+        if not index.isValid():
+            return False
+        row = index.row()
+        if index.column() > 0:
+            self.__data[row][index.column()] = value
+            self.__data[row][-1] = True
+            self.dataChanged.emit(index, index)
+            return True
+        return False
+    
+    def insertRows(self, row, count, parent=QModelIndex(), data=[]):
+        self.beginInsertRows(parent, row, row+len(data))
+        for d in data:
+            d.extend([None, False])
+            print('insertRows : {0}'.format(d))
+            self.__data.insert(row, d)
+        self.endInsertRows()
+        print('insertRows : {0}'.format(self.__data))
+        return True
+
+    def set_length(self, length, number, weight=0.0):
+        try:
+            idx = [l[0] for l in self.__data].index(length)
+            self.__data[idx][1] = number
+            self.__data[idx][2] = weight
+            self.__data[idx][-1] = True
+        except ValueError:
+            length_data = [length, number, weight]
+            self.insertRows(len(self.__data), 1, data=[length_data])
 
 
 
@@ -121,37 +182,33 @@ class Frm_Length(QWidget, frm_length.Ui_frm_length):
         super(Frm_Length, self).__init__()
         
         self.setupUi(self)
-
-        alpha_names, alpha_latin, alpha_aphiaid = [], [], []
-        with open('alpha_id.csv', newline='') as alpha_id_file:
-            spamreader = csv.reader(alpha_id_file, delimiter=',')
-            for row in spamreader:
-                alpha_names.append(row[1])
-                alpha_latin.append(row[2])
-                alpha_aphiaid.append(row[3])
+        
+        # alpha_names, alpha_latin, alpha_aphiaid = [], [], []
+        # with open('alpha_id.csv', newline='') as alpha_id_file:
+        #     spamreader = csv.reader(alpha_id_file, delimiter=',')
+        #     for row in spamreader:
+        #         alpha_names.append(row[1])
+        #         alpha_latin.append(row[2])
+        #         alpha_aphiaid.append(row[3])
                 
-        self.alpha_latin = dict(zip(alpha_names, alpha_latin))
-        self.alpha_aphiaid = dict(zip(alpha_names, alpha_aphiaid))
+        # self.alpha_latin = dict(zip(alpha_names, alpha_latin))
+        # self.alpha_aphiaid = dict(zip(alpha_names, alpha_aphiaid))
         
-        alpha_names = alpha_names[1:]
-        alpha_names.sort()
-        alpha_names +=  ["OTH"]
-        self.alpha_names = alpha_names
+        # alpha_names = alpha_names[1:]
+        # alpha_names.sort()
+        # alpha_names +=  ["OTH"]
+        # self.alpha_names = alpha_names
         
-        self.cb_latin.addItems(self.alpha_names)
+        # self.cb_latin.addItems(self.alpha_names)
         
         self.cb_fishCategory.addItems(["Landing", "Discard"])
         self.cb_lengthUnit.addItems(["cm", "cm below", "1/2 cm"])
         self.cb_weightUnit.addItems(["g", "kg", "t"])
         
-        self.cb_latin.activated.connect(self.activate)
-        
-        # self.sb_number.editingFinished.connect(self.insert_length)
-        # self.sb_number.editingFinished.connect(self.calc_all_number)
-        # self.btn_save.clicked.connect(self.save_data)
+        # self.cb_latin.activated.connect(self.activate)
         
 
-    def set_data(self):
+    def set_weight_data(self):
         
         self.haul_uid = ref.haul_uid
         self.weight_uid = None
@@ -167,9 +224,11 @@ class Frm_Length(QWidget, frm_length.Ui_frm_length):
         cur.close()
         
         if self.__data != None:
-            index = self.alpha_names.index(self.__data[2])
-            self.cb_latin.setCurrentIndex(index)
-            self.activate()
+            ref.weight_uid = self.__data[1]
+            
+            index = self.wgt_species.alpha_names.index(self.__data[2])
+            self.wgt_species.cb_latin.setCurrentIndex(index)
+            self.wgt_species.activate()
             
             if self.__data[5] == "L":
                 self.cb_fishCategory.setCurrentIndex(0)
@@ -196,20 +255,47 @@ class Frm_Length(QWidget, frm_length.Ui_frm_length):
             self.sb_number_sample.setValue(self.__data[9])
             
             self.weight_model = WeightTableModel()
-            self.length_model = LengthTableModel()
         
             self.tv_weight.setModel(self.weight_model)
-            self.tv_length.setModel(self.length_model)
-
-    def activate(self):
-        fishcode = self.cb_latin.itemText(self.cb_latin.currentIndex())
-        if fishcode == "OTH":
-            self.edit_aphiaid.setEnabled(1)
-            self.edit_latin.setEnabled(1)
-        else:
-            self.edit_aphiaid.setEnabled(0)
-            self.edit_latin.setEnabled(0)
             
-            self.edit_aphiaid.setText(self.alpha_aphiaid[fishcode])
-            self.edit_latin.setText(self.alpha_latin[fishcode])
+
+    # def activate(self):
+    #     fishcode = self.cb_latin.itemText(self.cb_latin.currentIndex())
+    #     if fishcode == "OTH":
+    #         self.edit_aphiaid.setEnabled(1)
+    #         self.edit_latin.setEnabled(1)
+    #     else:
+    #         self.edit_aphiaid.setEnabled(0)
+    #         self.edit_latin.setEnabled(0)
+            
+    #         self.edit_aphiaid.setText(self.alpha_aphiaid[fishcode])
+    #         self.edit_latin.setText(self.alpha_latin[fishcode])
+    
+    
+    def set_length_data(self):
+        
+        self.weight_uid = ref.weight_uid
+
+        cur = ref.connection.cursor()
+        
+        sql_statement = """SELECT * FROM com_new_final.sample_length WHERE
+        we_index = {};""".format(self.weight_uid)
+        
+        cur.execute(sql_statement)
+        
+        self.__data = cur.fetchone()
+        cur.close()
+        
+        ref.length_uid = self.__data[0]
+        
+        if self.__data != None:
+            
+            self.length_model = LengthTableModel()
+            
+            self.tv_length.setModel(self.length_model)
+            
+            self.sb_length_weight.setValue(self.__data[2])
+            self.sb_number.setValue(self.__data[3])
+            
+            
 
