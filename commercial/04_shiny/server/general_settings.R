72ffe9c6d8208583fe7ddb466decc99385a8db90
Sven Stötera <stoetera@ad.vti.bund.de>
2021-11-11 16:26:28 +0100

    disclaimer on data use

--- commercial/04_shiny/server/general_settings.R
+++ commercial/04_shiny/server/general_settings.R
@@ -10,3 +10,24 @@ shinyjs::onclick("sampleID",  updateTabsetPanel(session, inputId="navbar", selec
 shinyjs::onclick("stockID",  updateTabsetPanel(session, inputId="navbar", selected="Biology overview"))
 
 
+# --------------------------------
+# modal disclaimer
+# --------------------------------
+
+# the modal dialog where the user agree in terms
+disclaimer_modal <- modalDialog(
+  title = "Terms of use",
+  includeHTML("data/DescriptionDisclaimer.txt"),
+  easyClose = T, #set to F if you want people to click the "I agree" button
+  footer = tagList( downloadButton("report", "Download Disclaimer"),
+                    actionButton("ok", "I agree")
+  )     
+)
+
+# Show the modal on start up ...
+showModal(disclaimer_modal)
+
+# ... close when agree
+observeEvent(input$ok,{
+  removeModal()
+})
