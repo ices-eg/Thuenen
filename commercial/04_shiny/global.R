b2af86c6f2c90cec073f14a00e8af2f25e229dfe
Gilang Hardadi <37112628+ghardadi@users.noreply.github.com>
2021-11-22 14:49:01 +0100

    Update global.R include datasets

--- commercial/04_shiny/global.R
+++ commercial/04_shiny/global.R
@@ -38,6 +38,25 @@ library(mapplots)
 
 # data handling
 library(rintrojs)
-library(ggrepel) # required by shinyappsio
-library(rgeos) # required by shinyappsio
-library(sqldf)
+library(ggrepel) # requiered by shinyappsio
+library(rgeos) # requiered by shinyappsio
+
+ices.rect <- read_sf("shp/ices_rectangles/ices_squares_simple.shp")
+ices.rect<-as(ices.rect, 'Spatial')
+
+trip <- read.csv("data/trip.csv", sep=";")
+haul_fo <- read.csv("data/haul_fo.csv")
+haul_gear <- read.csv("data/haul_gear.csv")
+
+sample_weight <- read.csv("data/sample_weight.csv")
+sample_length <- read.csv("data/sample_length.csv")
+sample_bio <- read.csv("data/sample_bio.csv")
+
+landings <- read.csv("data/anlandung.csv", sep=";")
+
+# add species group to the landings 
+landings <- landings %>% mutate(group_fish = case_when(fischart %in% c("COD", "PLE", "FLE", "DAB", "SOL", "TUR", "BLL", "WHG", "LUM") ~ "demersal species", 
+                                                       fischart %in% c("HER", "SPR", "GAR", "MAC", "HKE") ~ "pelagic species", 
+                                                       fischart %in% c("FPE", "FRO", "FBR", "FBU", "FPI", "FPP", "FTE", "ELP", "FCC", "FCP") ~ "freshwater species",
+                                                       fischart %in% c("TRS","PLN", "ELE","SAL","TRO") ~ "freshwater species", TRUE ~ "other")
+)
