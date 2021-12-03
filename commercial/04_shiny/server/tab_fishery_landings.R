61f9fe5d6fceff442fefdca34daa7793d7caf592
Sven Stötera <stoetera@ad.vti.bund.de>
2021-12-03 10:17:44 +0100

    updates

--- commercial/04_shiny/server/tab_fishery_landings.R
+++ commercial/04_shiny/server/tab_fishery_landings.R
@@ -7,16 +7,16 @@
 #    
 ##### Interactive Map - fishing areas #####
 # Create the map - leaflet 
-output$map <- renderLeaflet({
-  leaflet() %>% 
-    addProviderTiles(providers$Esri.OceanBasemap) %>% 
-    setView(lng = 13.0000, lat = 54.800, zoom = 7)
-})
+#output$map <- renderLeaflet({
+#  leaflet() %>% 
+#    addProviderTiles(providers$Esri.OceanBasemap) %>% 
+#    setView(lng = 13.0000, lat = 54.800, zoom = 7)
+#})
 
 # Filtering for Mapping 
-mapdata1 <- reactive({
-  subset(mapdata, Year %in% input$slideryear)
-}) 
+#mapdata1 <- reactive({
+#  subset(mapdata, Year %in% input$slideryear)
+#}) 
 
   
   # Return the requested dataset ----
